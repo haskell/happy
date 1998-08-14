2353d1b9d3c9482923691a13ce50ebab593b73fc
sof <unknown>
1998-08-14 13:21:39 +0000

    [happy @ 1998-08-14 13:21:38 by sof] Happy specific version + config info

---
+++ mk/config.mk
@@ -0,0 +1,12 @@
+#
+# Happy project information
+#
+
+
+# what to include in a binary distribution
+HappyMainDir = happy
+HappyBinDistDirs = happy
+HappyBinDistDocs = happy/doc
+HappyBinDistShScripts = happy
+
+include version.mk
---
+++ mk/version.mk
@@ -0,0 +1,19 @@
+#
+# Project-specific version information.
+#
+# Note:
+#   this config file is intended to centralise all
+#   project version information. To bump up the version
+#   info on your package, edit this file and recompile
+#   all the dependents. This file lives in the source tree.
+#
+
+#
+# happy project variable settings:
+#
+HappyProjectName       = Happy
+HappyProjectNameShort  = happy
+HappyProjectVersion    = 1.5
+HappyProjectVersionInt = 15
+HappyProjectPatchLevel = 0
+
