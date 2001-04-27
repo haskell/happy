7d727d5c05125eb87426e9f0b8b981dee04e8405
simonmar <unknown>
2001-04-27 10:07:15 +0000

    [happy @ 2001-04-27 10:07:15 by simonmar] Include the 'happy' link in a binary dist.

--- mk/config.mk
+++ mk/config.mk
@@ -2,11 +2,11 @@
 # Happy project information
 #
 
-
 # what to include in a binary distribution
-HappyMainDir = happy
-HappyBinDistDirs = happy
-HappyBinDistDocs = happy/doc
-HappyBinDistShScripts = happy-$(ProjectVersion)
+HappyMainDir 		= happy
+HappyBinDistDirs 	= happy
+HappyBinDistDocs 	= happy/doc
+HappyBinDistShScripts 	= happy-$(ProjectVersion)
+HappyBinDistLinks 	= happy
 
 include $(HappyMainDir)/mk/version.mk
