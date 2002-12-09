bb8ad0726318630cb200cfc04f8cab54f0affd08
ijones <unknown>
2002-12-09 21:51:51 +0000

    [happy @ 2002-12-09 21:51:51 by ijones] Initial revision

---
+++ alex/mk/boilerplate.mk
@@ -0,0 +1,28 @@
+#-----------------------------------------------------------------------------
+# $Id: boilerplate.mk,v 1.1 2002/12/09 21:51:51 ijones Exp $
+
+# Begin by slurping in the boilerplate from one level up.
+# Remember, TOP is the top level of the innermost level
+# (FPTOOLS_TOP is the fptools top)
+
+-include $(TOP)/mk/version.mk
+
+# We need to set TOP to be the TOP that the next level up expects!
+# The TOP variable is reset after the inclusion of the fptools
+# boilerplate, so we stash TOP away first:
+ALEX_TOP := $(TOP)
+TOP:=$(TOP)/..
+
+include $(TOP)/mk/boilerplate.mk
+
+# Reset TOP
+TOP:=$(ALEX_TOP)
+
+# -----------------------------------------------------------------
+# Everything after this point
+# augments or overrides previously set variables.
+# -----------------------------------------------------------------
+
+-include $(TOP)/mk/paths.mk
+-include $(TOP)/mk/opts.mk
+-include $(TOP)/mk/suffix.mk
