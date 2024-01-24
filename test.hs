a214c02cf6a3e5fc302b2550247849e777ac8d87
Sebastian Graf <sebastian.graf@kit.edu>
2024-01-24 21:22:14 +0100

    Compile testsuite with -threaded (#264)
    Fixes #264.

--- test.hs
+++ test.hs
@@ -4,6 +4,6 @@ import System.Process (system)
 import System.Exit (exitWith)
 
 main = do
-  let jFlag = "-j" ++ show numCapabilities
+  let jFlag = "-j" ++ show numCapabilities -- to run tests in parallel, run `cabal test --test-options="+RTS -N"`
   let cmd = ["make", jFlag, "-k", "-C", "tests", "clean", "all"]
   system (intercalate " " cmd) >>= exitWith
