ee146d19bdaaf63517ab29e9ac30c273be2e91cb
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-01-25 11:41:28 +0100

    Readthedocs: add explicit requirement sphinx_rtd_theme
    Closes #269.

--- doc/conf.py
+++ doc/conf.py
@@ -46,10 +46,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
